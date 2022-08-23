d0e77b4ed9a9f8a7fca68ab144b273c65e4ec337
Sérgio Sobrinho <sergio.sobrinho@rgsistemas.com.br>
2022-08-23 14:30:25 -0300

    Projeto Finalizado

---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="footer">
+    <p><span>FOOD</span> Burger &copy; 2022</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped>
+#footer {
+  height: 150px;
+  background-color: #222;
+  border-top: 4px solid #111;
+  color: #fcba03;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  bottom: 0;
+  width: 100%;
+}
+
+
+</style>
